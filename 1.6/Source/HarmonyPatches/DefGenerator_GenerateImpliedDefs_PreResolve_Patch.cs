cf3b0e1defdee28666abf5dadfba08f1ea0fc0ab
Taranchuk <nfhfyxer@gmail.com>
2025-12-28 20:43:49 +0200

    dryad nuzzling fix

--- 1.6/Source/HarmonyPatches/DefGenerator_GenerateImpliedDefs_PreResolve_Patch.cs
+++ 1.6/Source/HarmonyPatches/DefGenerator_GenerateImpliedDefs_PreResolve_Patch.cs
@@ -30,7 +30,17 @@ namespace GojisDryadTweaks
                 if (lordDutyNode != null)
                 {
                     var index = thinkTreeDef.thinkRoot.subNodes.IndexOf(lordDutyNode);
-                    thinkTreeDef.thinkRoot.subNodes.Insert(index + 1, new JobGiver_Nuzzle());
+                    var conditionalHasFaction = new ThinkNode_ConditionalHasFaction();
+                    conditionalHasFaction.subNodes = new List<ThinkNode>();
+                    var chancePerHourNuzzle = new ThinkNode_ChancePerHour_Nuzzle();
+                    chancePerHourNuzzle.subNodes = new List<ThinkNode>();
+                    var tagger = new ThinkNode_Tagger();
+                    tagger.tagToGive = JobTag.Misc;
+                    tagger.subNodes = new List<ThinkNode>();
+                    tagger.subNodes.Add(new JobGiver_Nuzzle());
+                    chancePerHourNuzzle.subNodes.Add(tagger);
+                    conditionalHasFaction.subNodes.Add(chancePerHourNuzzle);
+                    thinkTreeDef.thinkRoot.subNodes.Insert(index + 1, conditionalHasFaction);
                 }
             }
         }
